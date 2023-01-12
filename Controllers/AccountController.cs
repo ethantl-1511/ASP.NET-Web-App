6fa1a4cd5a750a42e04e74e38b60d4a3c19e681c
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2023-01-12 14:37:36 -0600

    Create AccountController.cs

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using TheatreCMS3.Models;
+
+namespace TheatreCMS3.Controllers
+{
+    // HeadAuthorLogin method Blog-section testing purposes
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> HeadAuthorLogin(LoginViewModel model, string returnUrl)
+    {
+        // Set variables to HeadAuthor credentials, only for testing purposes!!
+        var headAuth = "HeadAuthor";
+        var password = "password";
+
+        var result = await SignInManager.PasswordSignInAsync(headAuth, password, model.RememberMe, shouldLockout: false);
+        switch (result)
+        {
+            case SignInStatus.Success:
+                return RedirectToLocal(returnUrl);
+            case SignInStatus.Failure:
+            default:
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
+        }
+    }
+}
